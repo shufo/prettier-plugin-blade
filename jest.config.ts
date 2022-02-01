ded56a63d4a7fe1f3cfa736edae612f7400ceff0
shufo <meikyowise@gmail.com>
2022-02-01 19:29:43 +0900

    ci: 🎡 generate coverage on CI

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+const ENABLE_COVERAGE = !!process.env.CI;
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
   cache: false,
+  collectCoverage: ENABLE_COVERAGE,
 };
