da9dc27a1c95c1c5f9ae2747c8bc62ffb71ac9fa
shufo <meikyowise@gmail.com>
2022-02-27 04:57:24 +0900

    style: 💄 apply formatter

--- __tests__/formatter.test.ts
+++ __tests__/formatter.test.ts
@@ -1,58 +1,77 @@
-import path from 'path';
-import fs from 'fs';
-import prettier from 'prettier';
+import path from "path";
+import fs from "fs";
+import prettier from "prettier";
 
-describe('formatter test', () => {
+describe("formatter test", () => {
   const fixturesDir = path.resolve(__dirname, "fixtures");
   const formattedFixturesDir = path.resolve(__dirname, "fixtures", "formatted");
-  const fixtureDirEntries = fs.readdirSync(fixturesDir, { withFileTypes: true });
+  const fixtureDirEntries = fs.readdirSync(fixturesDir, {
+    withFileTypes: true,
+  });
   const fixtures = fixtureDirEntries
-    .filter(entry => entry.isFile())
-    .map(entry => entry.name);
+    .filter((entry) => entry.isFile())
+    .map((entry) => entry.name);
 
-  fixtures.forEach(fixture => {
+  fixtures.forEach((fixture) => {
     test.concurrent(`can format fixture ${fixture}`, function () {
-      const content = fs.readFileSync(path.resolve(fixturesDir, fixture)).toString('utf-8');
+      const content = fs
+        .readFileSync(path.resolve(fixturesDir, fixture))
+        .toString("utf-8");
       const result = prettier.format(content, {
         plugins: [path.resolve(__dirname, "../")],
         parser: "blade",
         pluginSearchDirs: [path.resolve(__dirname, "../")],
       });
-      const expected = fs.readFileSync(path.resolve(formattedFixturesDir, `formatted.${fixture}`)).toString('utf-8');
+      const expected = fs
+        .readFileSync(
+          path.resolve(formattedFixturesDir, `formatted.${fixture}`)
+        )
+        .toString("utf-8");
       expect(result).toEqual(expected);
     });
-  })
+  });
 });
 
-describe('broken text test', () => {
+describe("broken text test", () => {
   const fixturesDir = path.resolve(__dirname, "error-fixtures");
 
   test.concurrent(`can not format fixture`, function () {
-    const content = fs.readFileSync(path.resolve(fixturesDir, "syntax.error.blade.php")).toString('utf-8');
+    const content = fs
+      .readFileSync(path.resolve(fixturesDir, "syntax.error.blade.php"))
+      .toString("utf-8");
     const f = () => {
       prettier.format(content, {
         plugins: [path.resolve(__dirname, "../")],
         parser: "blade",
         pluginSearchDirs: [path.resolve(__dirname, "../")],
-      })
+      });
     };
     expect(f).toThrowError("SyntaxError");
   });
 });
 
-describe('option test', () => {
+describe("option test", () => {
   const fixturesDir = path.resolve(__dirname, "fixtures");
-  const formattedFixturesDir = path.resolve(__dirname, "fixtures", "formattedWithOption");
+  const formattedFixturesDir = path.resolve(
+    __dirname,
+    "fixtures",
+    "formattedWithOption"
+  );
 
   test.concurrent(`can format fixture with options`, function () {
-    const content = fs.readFileSync(path.resolve(fixturesDir, "index.blade.php")).toString('utf-8');
+    const content = fs
+      .readFileSync(path.resolve(fixturesDir, "index.blade.php"))
+      .toString("utf-8");
     const result = prettier.format(content, {
       plugins: [path.resolve(__dirname, "../")],
       parser: "blade",
       pluginSearchDirs: [path.resolve(__dirname, "../")],
       tabWidth: 2,
     });
-    const expected = fs.readFileSync(path.resolve(formattedFixturesDir, `index.blade.php`)).toString('utf-8');
+    const expected = fs
+      .readFileSync(path.resolve(formattedFixturesDir, `index.blade.php`))
+      .toString("utf-8");
     expect(result).toEqual(expected);
   });
+
 });
