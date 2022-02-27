11e4f3a07f1b58cb503ab518ba32a97b7e495ddf
shufo <meikyowise@gmail.com>
2022-02-27 17:37:49 +0900

    Revert "test: 💍 fix test cases to follow current behaviour"
    This reverts commit e3b58f380d8148d6b9bf8766e1e9b7bc5e0105e4.

--- __tests__/fixtures/formatted/formatted.blade_comment_in_longline.blade.php
+++ __tests__/fixtures/formatted/formatted.blade_comment_in_longline.blade.php
@@ -113,12 +113,12 @@
                     </div>
                     <div class="col-sm-4">
 
-                        <div class="text-blue well well-sm text-center">
+                        <div class="well well-sm text-center text-blue">
                             <h2><strong>@lang('bon.your-points'): <br></strong>{{ $userbon }}</h2>
                         </div>
 
                         <div class="well well-sm mt-20">
-                            <p class="lead text-orange text-center">@lang('bon.exchange-warning')
+                            <p class="lead text-center text-orange">@lang('bon.exchange-warning')
                                 <br><strong>@lang('bon.no-refund')</strong>
                             </p>
                         </div>
