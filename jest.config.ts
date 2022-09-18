c589e3358243a854986f9cbca632cfde04b3c52d
shufo <meikyowise@gmail.com>
2022-09-19 07:10:46 +0900

    test: 💍 add test matcher

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,7 @@ const ENABLE_COVERAGE = !!process.env.CI;
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
   verbose: true,
   cache: false,
   collectCoverage: ENABLE_COVERAGE,
