3e079d7f85e081e7801ed7c3f464707f4aa57c81
Shuhei Hayashibara <meikyowise@gmail.com>
2023-07-04 00:46:08 +0900

    refactor: 💡 use ES module style import

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,5 @@
-const { runAsWorker } = require("synckit");
-const { BladeFormatter } = require("blade-formatter");
+import { runAsWorker } from "synckit";
+import { BladeFormatter } from "blade-formatter";
 
 runAsWorker(async (text, options) => {
   const result = await new BladeFormatter(options).format(text);
