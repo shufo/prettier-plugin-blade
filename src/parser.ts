d35c32475d9814708215ce2d1ba5ecc5f2256729
Yahav <Yahav@users.noreply.github.com>
2026-02-01 20:02:16 +0200

    Add endOfLine option to formatter options

--- src/parser.ts
+++ src/parser.ts
@@ -44,6 +44,7 @@ export const parse = async (
 		componentPrefix: opts.componentPrefix?.split(","),
 		// @ts-ignore
 		extraLiners: opts.extraLiners.split(","),
+		endOfLine: opts.endOfLine || "LF",
 		phpVersion: opts.phpVersion || "auto",
 	};
 
